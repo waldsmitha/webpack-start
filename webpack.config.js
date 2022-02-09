911fa13b479cf31c480ce30fc4bacd308aea4b08
Austin Waldsmith <austin.waldsmith@gmail.com>
2022-02-08 19:38:07 -0500

    added code to allow for building

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   // (1) Use the src/index.js file as entry point to bundle it. If the src/index.js file imports other JavaScript files, bundle them as well.
@@ -20,6 +22,17 @@ module.exports = {
     path: path.resolve(__dirname, "./public"),
     filename: "bundle.js",
   },
+  // Below plugins creates new default index.html in public every build, but if I want my own custom one, use the below code
+  // plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    //wipes public folder before creating new files
+    new CleanWebpackPlugin(),
+    //Allows us to specify html file to create
+    new HtmlWebpackPlugin({
+      title: "Hello Webpack bundled JavaScript Project",
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+  ],
   // (3) The /public folder will be used to serve our application to the browser.
   devServer: {
     static: path.resolve(__dirname, "./public"),
